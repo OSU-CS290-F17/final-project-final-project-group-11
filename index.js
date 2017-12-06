b14e73799fef66ef69584102ad35411bd1ff0441
a99606112006 <32274296+a99606112006@users.noreply.github.com>
2017-12-06 13:17:02 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+
+document.getElementById("book-car-button").onclick = function() {open()};
+document.getElementById("modal-close").onclick = function() {close()};
+document.getElementById("modal-accept").onclick = function() {accept()};
+document.getElementById("modal-cancel").onclick = function() {cancel()};
+document.getElementById("sell-something-button").onclick = function() {openfilter()};
+
+
+function open() {
+	document.getElementById("book-a-car").style.display = "flex";
+	document.getElementById("model-backdrop").style.display = "flex";
+	document.getElementById("filter-modal").style.display = "flex";
+	
+	}
+
+function close() {
+	document.getElementById("book-a-car").style.display = "none";	
+	}
+function accept() {
+	document.getElementById("book-a-car").style.display = "none";	
+	}
+function cancel() {
+	document.getElementById("book-a-car").style.display = "none";	
+	}
+
+function openfilter() {
+	document.getElementById("book-a-car").style.display = "flex";
+	document.getElementById("model-backdrop").style.display = "flex";
+	document.getElementById("filter-modal").style.display = "flex";
+	
+	}	
