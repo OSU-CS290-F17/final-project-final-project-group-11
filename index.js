024c63f8f1697203e50dd4bee7139ca90df70083
a99606112006 <32274296+a99606112006@users.noreply.github.com>
2017-12-06 13:50:15 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,31 +2,33 @@
 
 
 document.getElementById("book-car-button").onclick = function() {open()};
+
+
+document.getElementById("sell-something-button").onclick = function() {openfilter()};
 document.getElementById("modal-close").onclick = function() {close()};
 document.getElementById("modal-accept").onclick = function() {accept()};
 document.getElementById("modal-cancel").onclick = function() {cancel()};
-document.getElementById("sell-something-button").onclick = function() {openfilter()};
+
 
 
 function open() {
-	//document.getElementById("book-a-car").style.display = "flex";
-	//document.getElementById("modal-backdrop").style.display = "flex";
-	//document.getElementById("filter-modal").style.display = "flex";
-	
+	document.getElementById("book-a-car").style.display = "flex";	
 	}
 
 function close() {
-	document.getElementById("book-a-car").style.display = "none";	
+	document.getElementById("modal-backdrop").style.display = "none";
+	document.getElementById("filter-modal").style.display = "none";	
 	}
 function accept() {
-	document.getElementById("book-a-car").style.display = "none";	
+	document.getElementById("modal-backdrop").style.display = "none";
+	document.getElementById("filter-modal").style.display = "none";
 	}
 function cancel() {
-	document.getElementById("book-a-car").style.display = "none";	
+	document.getElementById("modal-backdrop").style.display = "none";
+	document.getElementById("filter-modal").style.display = "none";
 	}
 
-function openfilter() {
-	//document.getElementById("book-a-car").style.display = "flex";
+function openfilter() {	
 	document.getElementById("modal-backdrop").style.display = "flex";
 	document.getElementById("filter-modal").style.display = "flex";
 	
