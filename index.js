5397374fcfbfec50e97d6a4c724c700c2d926d12
a99606112006 <32274296+a99606112006@users.noreply.github.com>
2017-12-06 17:08:58 -0800

    Add files via upload
    except book, it can open and close

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 
+document.getElementById("book-car-button").addEventListener('click',OpenReserve);
+document.getElementById("modal-close2").onclick = function() {CloseReserve()};
+document.getElementById("modal-accept2").onclick = function() {AcceptReserve()};
+document.getElementById("modal-cancel2").onclick = function() {CancelReserve()};
+
+document.getElementById("sell-something-button").onclick = function() {Openfilter()};
+document.getElementById("modal-close").onclick = function() {Closefilter()};
+document.getElementById("modal-accept").onclick = function() {Acceptfilter()};
+document.getElementById("modal-cancel").onclick = function() {Cancelfilter()};
 
 
-document.getElementById("book-car-button").onclick = function() {open()};
 
-
-document.getElementById("sell-something-button").onclick = function() {openfilter()};
-document.getElementById("modal-close").onclick = function() {close()};
-document.getElementById("modal-accept").onclick = function() {accept()};
-document.getElementById("modal-cancel").onclick = function() {cancel()};
-
-
-
-function open() {
+function OpenReserve() {
 	document.getElementById("book-a-car").style.display = "flex";	
 	}
 
-function close() {
+function CloseReserve() {
+	document.getElementById("book-a-car").style.display = "none";
+	}
+	
+function AcceptReserve() {
+	document.getElementById("book-a-car").style.display = "none";
+	}
+
+function CancelReserve() {
+	document.getElementById("book-a-car").style.display = "none";
+	}
+	
+	
+function Closefilter() {
 	document.getElementById("modal-backdrop").style.display = "none";
 	document.getElementById("filter-modal").style.display = "none";	
 	}
-function accept() {
+function Acceptfilter() {
 	document.getElementById("modal-backdrop").style.display = "none";
 	document.getElementById("filter-modal").style.display = "none";
 	}
-function cancel() {
+function Cancelfilter() {
 	document.getElementById("modal-backdrop").style.display = "none";
 	document.getElementById("filter-modal").style.display = "none";
 	}
 
-function openfilter() {	
+function Openfilter() {	
 	document.getElementById("modal-backdrop").style.display = "flex";
 	document.getElementById("filter-modal").style.display = "flex";
 	
 	}	
+
+
+
